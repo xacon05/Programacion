18848cee3496173067a8f23551a6f4e46335eb2c
xacon05 <xacon005@gmail.com>
2025-06-02 12:51:03 +0200

    Delete Proyecto final con lo de base de datos directory

--- Proyecto final con lo de base de datos/NaveException.java
+++
@@ -1,8 +0,0 @@
-package Tema8;
-
-//Excepción personalizada para movimientos inválidos
-public class NaveException extends Exception {
- public NaveException(String mensaje) {
-     super(mensaje);
- }
-}
